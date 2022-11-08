87c6dfa552ef368ab43e43e631925b0ab52059fb
Jing Wang <jing@figur8.me>
2022-11-07 21:31:49 -0500

    Initial Commit

---
+++ NewLikeTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewLikeTimes
+//
+//  Created by Jing Wang on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
