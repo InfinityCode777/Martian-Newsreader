9ae9918328c65190e64a6648e6b9fd54dbed2806
Jing Wang <jing@figur8.me>
2022-11-10 23:30:04 -0500

    Detail view works *

--- NewLikeTimes/NewsDetail/NewsDetailVC.swift
+++ NewLikeTimes/NewsDetail/NewsDetailVC.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class NewsDetailVC: UIViewController {
+    // This should not be stored here
     var news: NewsViewModel!
     var presenter: NewsDetailPresenter!
+    
+    @IBOutlet var titelLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var bodyTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +35,8 @@ extension NewsDetailVC: NewsDetailPresenterOutput {
     func showNewsDetail() {
 //    func show(newsDetail: NewsViewModel) {
         print("ahaha >>> Title >>> \(news.title)")
+        titelLabel.text = news.title
+        bodyTextView.text = news.body
+        imageView.image = news.image
     }
 }
